0999b74b692e5653f22b56e71550c2ae423d8a68
jeplm2 <jeptoni@gmail.com>
2023-08-28 20:39:22 +0900

    Logging added. not yet ready.

---
+++ Ghost/src/Ghost/Log.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Ghost
+{
+    class GHOST_API::Log
+    {
+        public:
+            Log();
+            ~Log();
+
+            
+    }
+}
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ ./Sandbox/src/SandboxApp.cpp -o test ./Ghost/src/Ghost/Application.cpp
+g++ ./Sandbox/src/SandboxApp.cpp -o test ./Ghost/src/Ghost/Application.cpp -I./Ghost/vendor/spdlog/include/
 
 ./test
