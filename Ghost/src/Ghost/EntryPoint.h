2c9d3ccfd76b241912d051326d041894dc8c5abf
jeplm2 <your.jeptonni@gmail.com>
2023-08-30 07:33:46 +0900

    changed logging system

--- Ghost/src/Ghost/EntryPoint.h
+++ Ghost/src/Ghost/EntryPoint.h
@@ -1,11 +1,11 @@
 #pragma once
 
+
 Ghost::Application* Ghost::CreateApplication();
 int main(int argc, char** argv)
 {
-    Ghost::Log::Init();
-    GHOST_CORE_WARN("Initialized Log!");
-    GHOST_CORE_INFO("Hello!");
+    Ghost::Log log("Hey");
+
 
     auto app = Ghost::CreateApplication();
     app->run();
